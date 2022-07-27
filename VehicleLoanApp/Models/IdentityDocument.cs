599d57d49d1e8a9afc683ff6dca3b89087415bcb
abhishek1705 <abhiwa2000@gmail.com>
2022-07-27 17:32:33 +0530

    Add project files.

---
+++ VehicleLoanApp/Models/IdentityDocument.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace VehicleLoanApp.Models
+{
+    public partial class IdentityDocument
+    {
+        public int IdentityId { get; set; }
+        public byte[] Adharcard { get; set; }
+        public byte[] Pancard { get; set; }
+        public byte[] Photo { get; set; }
+        public byte[] Salaryslip { get; set; }
+        public int CustomerId { get; set; }
+
+        public virtual ApplicantDetail Customer { get; set; }
+    }
+}
---
+++ VehicleLoanApp/Models/LoanApplicationStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace VehicleLoanApp.Models
+{
+    public partial class LoanApplicationStatus
+    {
+        public LoanApplicationStatus()
+        {
+            LoanDetails = new HashSet<LoanDetail>();
+        }
+
+        public int StatusId { get; set; }
+        public string StatusName { get; set; }
+
+        public virtual ICollection<LoanDetail> LoanDetails { get; set; }
+    }
+}
